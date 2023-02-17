6f40ee0f4b851d36a99e222d5c51f7ee2ad3bbba
Alexander Paz <alexjpaz@gmail.com>
2023-02-17 10:57:24 -0500

    more tests

--- src/common.spec.js
+++ src/common.spec.js
@@ -4,13 +4,13 @@ const common = require('./common');
 
 describe('common', () => {
     describe('filterRomsFromPattern', () => {
-        it('should filter roms based on patters', () => {
+        it('should filter roms based on patterns', () => {
             const roms =  [
                 '1_Foo.nes',
                 '2_Bar.bin',
             ];
 
-            const pattern = "\.bin";
+            const pattern = "\.bin$";
 
             const result = roms.filter(common.filterRomsFromPattern(pattern));
 
@@ -18,7 +18,22 @@ describe('common', () => {
             expect(result.length).to.eql(1);
         });
 
-        it('should filter roms based on patters', () => {
+        it('should filter roms based on patterns', () => {
+            const roms =  [
+                '1_Foo.nes',
+                '2_Bar.bin',
+            ];
+
+            const pattern = ".bin$";
+
+            const result = roms.filter(common.filterRomsFromPattern(pattern));
+
+            expect(result[0]).to.eql('1_Foo.nes');
+            expect(result.length).to.eql(1);
+        });
+
+
+        it('should not filter if the pattern is null', () => {
             const roms =  [
                 '1_Foo.nes',
                 '2_Bar.bin',
@@ -37,7 +52,7 @@ describe('common', () => {
                 '2_Bar.bin',
             ];
 
-            const pattern = "\.foo";
+            const pattern = "\.foo$";
 
             const result = roms.filter(common.filterRomsFromPattern(pattern));
 
